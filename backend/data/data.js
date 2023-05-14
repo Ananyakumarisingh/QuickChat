99246018a7deec790cad0738fa5f5a28ccd4ccca
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-05-14 13:55:49 +0530

    �️ Dummy dat

---
+++ backend/data/data.js
@@ -0,0 +1,108 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Komal Mahato",
+        email: "komal@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "Komal Mahato",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Shruti Shaw",
+        email: "shruti@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Shruti Shaw",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Ananya",
+        email: "ananya@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Ananya",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Komal Mahato",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Shruti Shaw",
+        email: "shruti@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Shruti Shaw",
+      email: "shruti@example.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Gitu Par",
+        email: "jane@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Gitu Par",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Komal Mahato",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Shruti Shaw",
+        email: "shruti@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Shruti Shaw",
+      email: "shruti@example.com",
+    },
+  },
+];
