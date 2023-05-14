b423c9ba28cc45e139b4ec6f280f433421215786
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-05-14 22:32:30 +0530

    message model, edited user and chat model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -17,8 +17,7 @@ const userSchema = mongoose.Schema(
     pic: {
       type: String,
       require: true,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+      default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
   { timestamps: true }
