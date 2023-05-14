acbc75bba9f2819612058c89cedd5488006e2bdb
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-05-14 14:11:58 +0530

    created user's model

---
+++ backend/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
