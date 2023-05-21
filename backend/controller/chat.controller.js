33d802a7a6e0bc3d3e487c28d5d4fcfcfcc52053
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-05-21 14:55:49 +0530

    rename group controller

--- backend/controller/chat.controller.js
+++ backend/controller/chat.controller.js
@@ -97,7 +97,14 @@ const createGroupChat = asyncHandler( async(req, res) => {
 
 
 const renameGroup = asyncHandler( async(req, res) => {
-
+  const { chatId, chatName } = req.body;
+  const updatedChat = await ChatModel.findByIdAndUpdate(chatId, {chatName}, {new: true}).populate("users", "-password").populate("groupAdmin", "-password");
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
 });
 
 
